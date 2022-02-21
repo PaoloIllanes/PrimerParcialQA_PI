739c8b5efc542fc1fe59f75f7b12b955a674aa92
Paolo <oeuthfoierughi@gmail.com>
2022-02-21 08:41:19 -0400

    Segundo Commit

---
+++ src/main/java/ejercicio2/Permission.java
@@ -0,0 +1,15 @@
+package ejercicio2;
+
+public class Permission {
+    public boolean validUser(String user,String pwd){
+
+        //todo
+
+        return true;
+    }
+
+    public String getPermission(){
+        //todo
+        return"";
+    }
+}
---
+++ src/main/java/ejercicio2/ValidUser.java
@@ -0,0 +1,16 @@
+package ejercicio2;
+
+public class ValidUser {
+    Permission permission;
+
+    public ValidUser(Permission objetoMock){
+
+        permission =  objetoMock;
+
+    }
+
+    public String getPermission(){
+        String msg="";
+    return msg;
+    }
+}
